a87e72bcbb430428cac5f296ecc392f67fa3ce1b
Anna RAACH <annagraph92@gmail.com>
2024-06-11 13:56:32 +0200

    refacto code

--- src/components/Exo1/Exo1.jsx
+++ src/components/Exo1/Exo1.jsx
@@ -3,25 +3,27 @@ import {Text, Image, View, StyleSheet} from 'react-native';
 const Exo1 = () => {
   return (
     <>
-      <Image
-        style={styles.image}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
+      <View>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+      </View>
       <View style={styles.divText}>
         <Text>
-          Nom : <Text style={styles.bold}> Capitaine</Text>
+          Nom : <Text style={styles.bold}>Capitaine</Text>
         </Text>
         <Text>
-          Prénom : <Text style={styles.bold}> Haddock</Text>
+          Prénom : <Text style={styles.bold}>Haddock</Text>
         </Text>
         <Text>
-          Téléphone : <Text style={styles.bold}> 01.23.45.67.89</Text>
+          Téléphone : <Text style={styles.bold}>01.23.45.67.89</Text>
         </Text>
         <Text>
           Adresse :{' '}
-          <Text style={styles.bold}> 35 rue des pias 59200 Tourcoing</Text>
+          <Text style={styles.bold}>35 rue des pias 59200 Tourcoing</Text>
         </Text>
       </View>
     </>
@@ -35,7 +37,8 @@ const styles = StyleSheet.create({
     borderRadius: 37.5,
   },
   divText: {
-    margin: 30,
+    margin: 10,
+    alignItems: 'center',
   },
   bold: {
     fontWeight: 'bold',
